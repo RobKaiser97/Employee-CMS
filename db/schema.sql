61e4acf18cb8421c03fbdea6ec7fe33775959e50
Robert Kaiser <paintballmanrob@gmail.com>
2023-09-17 20:48:09 -0400

    trim trailing whitespace

--- db/schema.sql
+++ db/schema.sql
@@ -1,29 +1,27 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
-
 USE company_db;
-
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
-
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,2) NOT NULL,
-    department_id INT NOT NULL,
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10, 2),
+    department_id INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE
+    SET NULL
 );
-
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT NOT NULL,
-    manager_id INT,
+    role_id INT,
+    manager_id INT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id),
-)
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,15 @@
-INSERT INTO department (name) VALUES ('Sales'), ('Engineering'), ('HR');
-
-INSERT INTO role (title, salary, department_id) VALUES 
-('Sales Manager', 100000, (SELECT id FROM department WHERE name = 'Sales')),
-('Software Engineer', 120000, (SELECT id FROM department WHERE name = 'Engineering')),
-('HR Manager', 80000, (SELECT id FROM department WHERE name = 'HR'));
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
-('John', 'Doe', (SELECT id FROM role WHERE title = 'Sales Manager'), NULL),
-('Jane', 'Smith', (SELECT id FROM role WHERE title = 'Software Engineer'), NULL),
-('Emily', 'Johnson', (SELECT id FROM role WHERE title = 'HR Manager'), (SELECT id FROM employee WHERE first_name = 'John' AND last_name = 'Doe'));
-
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
-('Alice', 'Brown', (SELECT id FROM role WHERE title = 'Sales Manager'), (SELECT id FROM employee WHERE first_name = 'John' AND last_name = 'Doe')),
-('Bob', 'Green', (SELECT id FROM role WHERE title = 'Software Engineer'), (SELECT id FROM employee WHERE first_name = 'Jane' AND last_name = 'Smith')),
-('Charlie', 'White', (SELECT id FROM role WHERE title = 'HR Manager'), (SELECT id FROM employee WHERE first_name = 'Emily' AND last_name = 'Johnson'));
+INSERT INTO department (name)
+VALUES ('Sales'),
+    ('Engineering'),
+    ('HR');
+INSERT INTO role (title, salary, department_id)
+VALUES ('Manager', 100000, 1),
+    ('Software Engineer', 120000, 2),
+    ('HR', 80000, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, 1),
+    ("Jane", "Smith", 2, 1),
+    ("Jack", "Jackson", 3, 1),
+    ("Mike", "Hunt", 2, NULL),
+    ("Sally", "Ride", 3, NULL),
+    ("Bob", "Barker", 1, NULL);
